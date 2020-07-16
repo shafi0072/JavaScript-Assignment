fdb670c4329d41386bdef6e2107fd514b40ae529
shafidark <safi29317@gmail.com.com>
2020-07-17 00:41:34 +0600

    shafi

--- assignment.js
+++ assignment.js
@@ -4,7 +4,7 @@ function feetToMile(Feet) {
     let Mile = feet * 0.00018939393;
     return Mile;
 }
-let mileResult = feetToMile(Feet);
+let mileResult = feetToMile(200);
 console.log(mileResult);
 
 
@@ -17,5 +17,24 @@ function woodCalculator(chair, table, bed){
     let totalCubicFeet = totalSum / 1728;
     return totalCubicFeet;
 }
-let woodCalculatorResult = woodCalculator(chair, table, bed);
+let woodCalculatorResult = woodCalculator(1, 4, 2);
 console.log(woodCalculatorResult);
+
+
+/*brickCalculator*/
+function brickCalculator(Floor){
+    let brick = 1000; 
+    if(Floor<=10){
+        let brickEqual =  brick * 15;
+        return brickEqual
+    }
+    else if(Floor <= 100){
+        let brickEqual = brick * 17;
+        return brickEqual
+    }
+    else{
+        let brickEqual = brick * 10;
+    }
+}
+let brickCalculatorResult = brickCalculator(20);
+console.log(brickCalculatorResult);
